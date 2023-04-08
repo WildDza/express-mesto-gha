d26d6c1b2a943fab943c5c644f13606d1689b8e0
LAPTOP-CNI641BD\WildD <Kleha91@yandex.ru>
2023-04-08 10:12:46 +0700

    fix: подготовка к выполнению ПР 13

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+// const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+// const routes = require("./routes/index");
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+mongoose.connect("mongodb://localhost:27017/mestodb");
+
+//   , {
+//   useNewUrlParser: true,
+//   useCreateIndex: true,
+//     useFindAndModify: false
+// });
+
+app.listen(PORT, () => {
+  console.log(
+    `The application listens on the port from which the server is started: http://localhost:${PORT}`
+  );
+});
