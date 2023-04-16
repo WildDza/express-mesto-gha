bd768ca0acd82bbefd5d6f562560df3f36915319
LAPTOP-CNI641BD\WildD <Kleha91@yandex.ru>
2023-04-16 19:52:16 +0700

    test hotfix

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
-const express = require('express');
+// const express = require('express');
 const router = require('express').Router();
 const usersRoutes = require('./users');
 const cardsRoutes = require('./cards');
 const Codes = require('../utils/codes');
 const auth = require('../middlewares/auth');
 
-const app = express();
-app.use(auth);
+// const app = express();
+// app.use(auth);
 
-router.use('/users', usersRoutes);
-router.use('/cards', cardsRoutes);
+router.use('/users', auth, usersRoutes);
+router.use('/cards', auth, cardsRoutes);
 
 router.use((req, res) => {
   res.status(Codes.Not_Found).send({ message: 'Страница не найдена' });
