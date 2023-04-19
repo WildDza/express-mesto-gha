443105adbd9341ad76deccd96c357a04a06d02ea
LAPTOP-CNI641BD\WildD <Kleha91@yandex.ru>
2023-04-19 22:47:59 +0700

    hotfix errors

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,12 @@
-// const express = require('express');
 const router = require('express').Router();
 const usersRoutes = require('./users');
 const cardsRoutes = require('./cards');
-// const Codes = require('../utils/codes');
 const auth = require('../middlewares/auth');
 const { NotFoundErr } = require('../errors/NotFoundErr');
-
-// const app = express();
-// app.use(auth);
 
 router.use('/users', auth, usersRoutes);
 router.use('/cards', auth, cardsRoutes);
 
 router.use('*', (req, res, next) => next(new NotFoundErr('Страница не найдена')));
-// res.status(Codes.Not_Found).send({ message: 'Страница не найдена' });
 
 module.exports = router;
